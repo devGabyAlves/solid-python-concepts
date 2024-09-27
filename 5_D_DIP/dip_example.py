5a1c61ee32607f1001989337606cb9497b48d4a8
devGabyAlves <gabriellyalves401@gmail.com>
2024-09-27 00:16:48 -0300

    docs: add explanation of the Dependency Inversion principle (DIP) in the file

--- 5_D_DIP/dip_example.py
+++ 5_D_DIP/dip_example.py
@@ -1,3 +1,10 @@
+# D - Principio da Inversao da Dependencia (DIP)
+""""
+Modulos de alto nivel nao devem depender diretamente dos modeulos de baixo nivel
+
+Os sistemas mais flexiveis sao aqueles em que as dependencias do codigo-fonte referem-se apenas a abstracoes, nao a concrecoes
+"""
+
 from .notificator_interface import NotificatorInterface
 
 
